6c5220019cef24abf7b2823d45878f11c7ceb86a
Yevhen Cherkes <ycherkes@outlook.com>
2024-08-17 18:30:49 +0200

    reference classic fuzzysharp in benchmarks

--- FuzzySharp.Benchmarks/BenchmarkAll.cs
+++ FuzzySharp.Benchmarks/BenchmarkAll.cs
@@ -1,5 +1,8 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Raffinert.FuzzySharp.PreProcess;
+using RaffinertFuzz = Raffinert.FuzzySharp.Fuzz;
+using ClassicFuzz = FuzzySharp.Fuzz;
+using ClassicPreprocess = FuzzySharp.PreProcess;
 
 namespace Raffinert.FuzzySharp.Benchmarks;
 
@@ -9,84 +12,168 @@ public class BenchmarkAll
     [Benchmark]
     public int Ratio1()
     {
-        return Fuzz.Ratio("mysmilarstring", "myawfullysimilarstirng");
+        return RaffinertFuzz.Ratio("mysmilarstring", "myawfullysimilarstirng");
     }
 
     [Benchmark]
     public int Ratio2()
     {
-        return Fuzz.Ratio("mysmilarstring", "mysimilarstring");
+        return RaffinertFuzz.Ratio("mysmilarstring", "mysimilarstring");
     }
 
     [Benchmark]
     public int PartialRatio()
     {
-        return Fuzz.PartialRatio("similar", "somewhresimlrbetweenthisstring");
+        return RaffinertFuzz.PartialRatio("similar", "somewhresimlrbetweenthisstring");
     }
 
     [Benchmark]
     public int TokenSortRatio()
     {
-        return Fuzz.TokenSortRatio("order words out of", "  words out of order");
+        return RaffinertFuzz.TokenSortRatio("order words out of", "  words out of order");
     }
 
     [Benchmark]
     public int PartialTokenSortRatio()
     {
-        return Fuzz.PartialTokenSortRatio("order words out of", "  words out of order");
+        return RaffinertFuzz.PartialTokenSortRatio("order words out of", "  words out of order");
     }
 
     [Benchmark]
     public int TokenSetRatio()
     {
-        return Fuzz.TokenSetRatio("fuzzy was a bear", "fuzzy fuzzy fuzzy bear");
+        return RaffinertFuzz.TokenSetRatio("fuzzy was a bear", "fuzzy fuzzy fuzzy bear");
     }
 
     [Benchmark]
     public int PartialTokenSetRatio()
     {
-        return Fuzz.PartialTokenSetRatio("fuzzy was a bear", "fuzzy fuzzy fuzzy bear");
+        return RaffinertFuzz.PartialTokenSetRatio("fuzzy was a bear", "fuzzy fuzzy fuzzy bear");
     }
 
     [Benchmark]
     public int WeightedRatio()
     {
-        return Fuzz.WeightedRatio("The quick brown fox jimps ofver the small lazy dog", "the quick brown fox jumps over the small lazy dog");
+        return RaffinertFuzz.WeightedRatio("The quick brown fox jimps ofver the small lazy dog", "the quick brown fox jumps over the small lazy dog");
     }
 
     [Benchmark]
     public int TokenInitialismRatio1()
     {
-        return Fuzz.TokenInitialismRatio("NASA", "National Aeronautics and Space Administration");
+        return RaffinertFuzz.TokenInitialismRatio("NASA", "National Aeronautics and Space Administration");
     }
 
     [Benchmark]
     public int TokenInitialismRatio2()
     {
-        return Fuzz.TokenInitialismRatio("NASA", "National Aeronautics Space Administration");
+        return RaffinertFuzz.TokenInitialismRatio("NASA", "National Aeronautics Space Administration");
     }
 
     [Benchmark]
     public int TokenInitialismRatio3()
     {
-        return Fuzz.TokenInitialismRatio("NASA", "National Aeronautics Space Administration, Kennedy Space Center, Cape Canaveral, Florida 32899");
+        return RaffinertFuzz.TokenInitialismRatio("NASA", "National Aeronautics Space Administration, Kennedy Space Center, Cape Canaveral, Florida 32899");
     }
 
     [Benchmark]
     public int PartialTokenInitialismRatio()
     {
-        return Fuzz.PartialTokenInitialismRatio("NASA", "National Aeronautics Space Administration, Kennedy Space Center, Cape Canaveral, Florida 32899");
+        return RaffinertFuzz.PartialTokenInitialismRatio("NASA", "National Aeronautics Space Administration, Kennedy Space Center, Cape Canaveral, Florida 32899");
     }
 
     [Benchmark]
     public int TokenAbbreviationRatio()
     {
-        return Fuzz.TokenAbbreviationRatio("bl 420", "Baseline section 420", PreprocessMode.Full);
+        return RaffinertFuzz.TokenAbbreviationRatio("bl 420", "Baseline section 420", PreprocessMode.Full);
     }
 
     [Benchmark]
     public int PartialTokenAbbreviationRatio()
     {
-        return Fuzz.PartialTokenAbbreviationRatio("bl 420", "Baseline section 420", PreprocessMode.Full);
+        return RaffinertFuzz.PartialTokenAbbreviationRatio("bl 420", "Baseline section 420", PreprocessMode.Full);
+    }
+
+    [Benchmark]
+    public int Ratio1Classic()
+    {
+        return ClassicFuzz.Ratio("mysmilarstring", "myawfullysimilarstirng");
+    }
+
+    [Benchmark]
+    public int Ratio2Classic()
+    {
+        return ClassicFuzz.Ratio("mysmilarstring", "mysimilarstring");
+    }
+
+    [Benchmark]
+    public int PartialRatioClassic()
+    {
+        return ClassicFuzz.PartialRatio("similar", "somewhresimlrbetweenthisstring");
+    }
+
+    [Benchmark]
+    public int TokenSortRatioClassic()
+    {
+        return ClassicFuzz.TokenSortRatio("order words out of", "  words out of order");
+    }
+
+    [Benchmark]
+    public int PartialTokenSortRatioClassic()
+    {
+        return ClassicFuzz.PartialTokenSortRatio("order words out of", "  words out of order");
+    }
+
+    [Benchmark]
+    public int TokenSetRatioClassic()
+    {
+        return ClassicFuzz.TokenSetRatio("fuzzy was a bear", "fuzzy fuzzy fuzzy bear");
+    }
+
+    [Benchmark]
+    public int PartialTokenSetRatioClassic()
+    {
+        return ClassicFuzz.PartialTokenSetRatio("fuzzy was a bear", "fuzzy fuzzy fuzzy bear");
+    }
+
+    [Benchmark]
+    public int WeightedRatioClassic()
+    {
+        return ClassicFuzz.WeightedRatio("The quick brown fox jimps ofver the small lazy dog", "the quick brown fox jumps over the small lazy dog");
+    }
+
+    [Benchmark]
+    public int TokenInitialismRatio1Classic()
+    {
+        return ClassicFuzz.TokenInitialismRatio("NASA", "National Aeronautics and Space Administration");
+    }
+
+    [Benchmark]
+    public int TokenInitialismRatio2Classic()
+    {
+        return ClassicFuzz.TokenInitialismRatio("NASA", "National Aeronautics Space Administration");
+    }
+
+    [Benchmark]
+    public int TokenInitialismRatio3Classic()
+    {
+        return ClassicFuzz.TokenInitialismRatio("NASA", "National Aeronautics Space Administration, Kennedy Space Center, Cape Canaveral, Florida 32899");
+    }
+
+    [Benchmark]
+    public int PartialTokenInitialismRatioClassic()
+    {
+        return ClassicFuzz.PartialTokenInitialismRatio("NASA", "National Aeronautics Space Administration, Kennedy Space Center, Cape Canaveral, Florida 32899");
+    }
+
+    [Benchmark]
+    public int TokenAbbreviationRatioClassic()
+    {
+        return ClassicFuzz.TokenAbbreviationRatio("bl 420", "Baseline section 420", ClassicPreprocess.PreprocessMode.Full);
+    }
+
+    [Benchmark]
+    public int PartialTokenAbbreviationRatioClassic()
+    {
+        return ClassicFuzz.PartialTokenAbbreviationRatio("bl 420", "Baseline section 420", ClassicPreprocess.PreprocessMode.Full);
     }
 }
