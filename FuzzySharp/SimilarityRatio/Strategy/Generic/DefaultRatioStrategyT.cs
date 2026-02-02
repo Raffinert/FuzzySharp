919a46fa1a3bf7e9b48685d6ecc3678e5d80cca4
ycherkes <ycherkes@outlook.com>
2026-02-02 22:08:27 +0100

    extract one more cached strategy to its own file

--- FuzzySharp/SimilarityRatio/Strategy/Generic/DefaultRatioStrategyT.cs
+++ FuzzySharp/SimilarityRatio/Strategy/Generic/DefaultRatioStrategyT.cs
@@ -16,26 +16,3 @@ internal static class DefaultRatioStrategy<T> where T : IEquatable<T>
         return result;
     }
 }
-
-internal class CachedDefaultRatioStrategy<T>(T[] input1) : IDisposable
-    where T : IEquatable<T>
-{
-    private readonly IndelT<T> _indel = new(input1);
-
-    public int Calculate(T[] input2)
-    {
-        if (input1.Length == 0 || input2.Length == 0)
-        {
-            return 0;
-        }
-
-        var result = (int)Math.Round(100 * _indel.NormalizedSimilarityWith(input2));
-
-        return result;
-    }
-
-    public void Dispose()
-    {
-        _indel.Dispose();
-    }
-}
